7bb312d7cf0d946eb9d86210d3e7cc76a8059a2e
George Dimitrov <george.dimitrov@ludotech.co>
2022-10-04 09:42:26 +0200

    revert

--- Class 06 - Routing/routing_demo/src/components/Home/Home.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export const Home = () => {
-  return <h1>Welcome from home component</h1>;
-};
--- Class 06 - Routing/routing_demo/src/components/Recipes/add-recipe/AddRecipe.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export const AddRecipe = () => {
-  return <h1>Welcome from Add Recipe component</h1>;
-};
